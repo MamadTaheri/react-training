0584e39d21915c4d7c8a9371da7f02d01cb6c399
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-30 00:36:42 +0430

    let && var && const

---
+++ script.js
@@ -0,0 +1,56 @@
+// var i = 5;
+// console.log(i);
+
+// var i = 6;
+// console.log(i);
+
+// let i = 5;
+// console.log(i);
+
+// let i = 6;
+// console.log(i);
+
+// const i = 5;
+// console.log(i);
+
+// const i = 6;
+// console.log(i);
+
+// if(true) {
+//     var i = 5;
+//     console.log(i)
+// }
+
+// console.log(i)
+
+// if(true) {
+//     let i = 5;
+//     console.log(i)
+// }
+
+// console.log(i)
+
+// if(true) {
+//     const i = 5;
+//     console.log(i)
+// }
+
+// console.log(i)
+
+// let i = 5;
+// console.log(i);
+
+// i=6;
+// console.log(i)
+
+// const i = 5;
+// console.log(i);
+
+// i=6;
+// console.log(i)
+
+const i = [1, 2, 3];
+console.log(i);
+
+i.push(4);
+console.log(i) 
