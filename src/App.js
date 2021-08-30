b8783842a50511e040361a551c46dc2d4bf6efc6
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-30 09:38:26 +0430

    working with class component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 
 class App extends Component {
     render(){
-        return <h1>Mamad Taheri</h1>
+        return (
+            <>
+                <div className="container">
+                   <h1>Mamad Taheri</h1>
+                </div>
+                   <p>Hello world</p>
+               
+            </>
+        )
+        // return React.createElement("h1",null,"mamali");
+        // return React.createElement("h1",null,React.createElement("h1",null,"mamahtjrtjli"));
     }
 }
 
