203d268ab4c39e1ba31620f0c4e9c64100f20f21
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-30 18:12:11 +0430

    send method via props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import ClassEvent from './ClassEvent'
 // import FunctionEvent from './FunctionEvent'
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      text: "Mamad Taheri"
+    }
+  }
+
+  sayHi = () => {
+    console.log(this.state.text)
+  }
+
   render() {
     return (
       <div>
-        <ClassEvent />
+        <ClassEvent sayHi={this.sayHi}/>
         {/* <FunctionEvent /> */}
       </div>
     )
