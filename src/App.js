f6eef2c163e5b61afe975365b555a3bc9e235b23
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-30 13:49:26 +0430

    setState

--- src/App.js
+++ src/App.js
@@ -4,29 +4,32 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Benz",
-      speed: 250,
+       number : 0,
     };
+  } 
+
+  upOne = () => {
+    //   this.setState({
+    //       number: this.state.number + 1,
+    //   }, () => {console.log(`new number is : ${this.state.number}`)})
+    //   console.log(this.state.number)
+
+    this.setState((prevState) => ({
+        number: prevState.number + 1,
+    }))
   }
 
-  changeHandler = () => {
-      const carName = this.state.name == "Benz" ? "BMW" : "Benz"
-      const carSpeed = this.state.speed == 250 ? 300 : 250
-      this.setState({
-          name: carName,
-          speed: carSpeed
-      })
+  upThree = () => {
+      this.upOne();
+      this.upOne();
+      this.upOne();
   }
 
   render() {
     return (
       <div>
-        <h1>My cars:</h1>
-        <p>
-          My first car is {this.state.name} and it's top speed is{" "}
-          {this.state.speed}
-        </p>
-        <button onClick={this.changeHandler}>Change</button>
+         <h1>{this.state.number}</h1>
+         <button onClick={this.upThree}>UP 3</button>
       </div>
     );
   }
