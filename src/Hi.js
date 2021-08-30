d489f45eb2b5de788301cc181d4f3983deafc1d7
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-30 11:28:18 +0430

    using functional component

--- src/Hi.js
+++ src/Hi.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Hi extends Component {
-    render() { 
-        return ( 
-            <div>
-               <h1>Hi Mamad</h1>
-               <p>Welcome to Here</p>
-            </div>
-         );
-    }
+// class Hi extends Component {
+//     render() {
+//         return (
+//             <div>
+//                <h1>Hi Mamad</h1>
+//                <p>Welcome to Here</p>
+//             </div>
+//          );
+//     }
+// }
+
+const Hi = () => {
+  return (
+    <div>
+      <h1>Hi Mamad</h1>
+      <p>Welcome to Here</p>
+    </div>
+  );
 }
- 
+
 export default Hi;
