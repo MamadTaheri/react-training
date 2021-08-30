854794e9bd6483bfa22700007fed3472468be4fe
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-30 11:38:07 +0430

    dynamic content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
-import Hi from "./Hi";
 
 class App extends Component {
+    number = 10;
   render() {
     return (
       <>
-        <div className="container">
-          <h1>Mamad Taheri</h1>
-          <Hi />
-        </div>
-        <Hi />
-        <p>Hello world</p>
-        <Hi />
+        <p>Multiple: 4*5 </p>
+        <p>Multiple: {4 * 5} </p>
+        <p>Multiple:  Math.ceil(Math.random()*1000)  </p>
+        <p>Multiple:  {Math.ceil(Math.random()*1000)}  </p>
+        <p>{this.number} </p>
+        {console.log(this.number)}
       </>
     );
   }
