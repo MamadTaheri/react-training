4458e00c32f167d81b11dc2671be9ed5c45b0657
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-31 11:44:51 +0430

    create components for routing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import React, { Component } from "react";
-
-import axios from "axios";
-import Post from "./components/Post";
-import SendPost from "./components/SendPost";
+import React, { Component } from 'react'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      postData: [],
-    };
-  }
- 
-  componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => this.setState({postData: response.data}))
-  }
-
-  deletePost = (id) => {
-    axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then(response => console.log(response))
-  }
- 
   render() {
-    const {postData} = this.state
     return (
       <div>
-        <SendPost />
-        <h1>Posts: </h1>
-        {postData.map((post) => <Post key={post.id} deletePost={() => this.deletePost(post.id)} title={post.title} body={post.body}/> )}
+        
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
