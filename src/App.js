c3c1c93a2369502f2581e1eef638d2423ec3e6c2
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-31 10:59:16 +0430

    post data using axios

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import axios from "axios";
 import Post from "./components/Post";
+import SendPost from "./components/SendPost";
 
 class App extends Component {
   constructor(props) {
@@ -20,11 +21,7 @@ class App extends Component {
     const {postData} = this.state
     return (
       <div>
-        {/* <button onClick={this.getPost}>Get post</button> */}
-        <br />
-        {/* <button onClick={this.sendPost}>Post</button> */}
-        <br />
-        {/* <button onClick={this.deletePost}>Delete</button> */}
+        <SendPost />
         <h1>Posts: </h1>
         {postData.map((post) => <Post key={post.id} title={post.title} body={post.body}/> )}
       </div>
