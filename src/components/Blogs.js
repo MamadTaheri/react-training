701058f29d83b979144fb722bc42b073d9d9a871
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-08-31 13:07:17 +0430

    using query string

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import queryString from 'query-string';
 
 class Blogs extends Component {
+
+    parseQuery = () => {
+        console.log(this.props.location.search)
+        const result = queryString.parse(this.props.location.search)
+        console.log(result)
+    }
+
     render() {
         return (
             <div>
                 <h1>Blogs</h1>
                 <p>Thw auther is : {this.props.match.params.author}</p>
+                <button onClick={this.parseQuery}>Show</button>
             </div>
         );
     }
