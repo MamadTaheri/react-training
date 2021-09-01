5a4818d7572fb163c6f3b5f65b748c715d93740b
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-01 09:47:50 +0430

    more advance useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Counter from "./components/Counter";
+import NewCounter from "./components/NewCounter";
 
 
 const App = () => {
 
   return (
     <div>
-      <Counter />
+      <NewCounter />      
     </div>
   );
 };
