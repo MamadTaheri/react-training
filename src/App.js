86e0f610c2b8a333156395eb747173fe851688c2
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-01 09:31:59 +0430

    useReducer in func comp

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
+import Counter from "./components/Counter";
 
-import CompA from "./components/CompA";
-
-export const NameContext = React.createContext();
-export const AgeContext = React.createContext();
 
 const App = () => {
-  const [name, setName] = useState("Milad");
 
   return (
     <div>
-      <NameContext.Provider value={name}>
-        <AgeContext.Provider value={24}>
-          <CompA name={name} />
-        </AgeContext.Provider>
-      </NameContext.Provider>
+      <Counter />
     </div>
   );
 };
