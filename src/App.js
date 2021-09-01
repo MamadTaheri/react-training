17bc21bb0297f91d621eb383297381e49326bf1c
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-01 16:48:16 +0430

    useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
-import NewGetData from "./components/NewGetData";
-
+import React, { useState, useMemo } from 'react';
 
 const App = () => {
 
+  const [isShown, setIsShown] = useState(true);
+  const [number, setNumber] = useState(0);
+
+  const slow =useMemo(() => {
+    for (let i=0; i<1000000000; i++) {}
+    return isShown;
+  }, [isShown])
+
+  const showHandler = () => {
+    setIsShown(!isShown);
+  }
+
+  const numberHandler = () => {
+    setNumber(prevNumber => prevNumber + 1);
+  }
+
   return (
     <div>
-      <NewGetData />     
+      <button onClick={showHandler}>show/hide</button>
+      <span>{slow ? "Show" : "Hide"}</span>
+      <br/>
+      <button onClick={numberHandler}>+</button>
+      <span>{number}</span>
     </div>
   );
 };
