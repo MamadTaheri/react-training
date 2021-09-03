c4e8c59379cc423fbff70cd4651101fa6e8f1cf0
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-03 11:39:19 +0430

    refresh project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { useState } from 'react';
-import useLocalStorage from './hooks/useLocalStorage';
+import React from 'react';
 
 const App = () => {
-
-  const [name, setName] = useLocalStorage("name", "")
-
   return (
     <div>
-       <input type="text" value={name} onChange={event => setName(event.target.value)} />
+      
     </div>
   );
 };
