6e4aca73c7f124324a5ee9049bcef3566f79e8a6
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-04 01:03:03 +0430

    Refresh Project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import './App.css'
-import { Redirect, Route, Switch } from 'react-router-dom';
-import Login from './components/auth/login/Login'
-
-import SignUp from "./components/auth/signup/SignUp"
-
 
 const App = () => {
   return (
     <div className="App">
-       <Switch>
-         <Route path="/login" component={Login} />
-         <Route path="/signup" component={SignUp} />
-         <Redirect from="/" to="/login" />
-       </Switch>
+       App
     </div>
   );
 };
