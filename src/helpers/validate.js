66810579a81172f94b79287b3c5ab4eac0c8167c
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-03 21:10:21 +0430

    create validation helper

---
+++ src/helpers/validate.js
@@ -0,0 +1,41 @@
+const validate = data => {
+    const errors = {};
+
+    if (!data.name.trim()) {
+        errors.name = "Username required"
+    } else {
+        delete errors.name
+    }
+
+    if(!data.email) {
+        errors.email = "Email required"
+    } else if(!/\S+@\S+\.\S+/.test(data.email)) {
+        errors.email = "Email address is invalid"
+    } else {
+        delete errors.email
+    }
+
+    if(!data.password) {
+        errors.password = "Password is required"
+    } else if(data.password.length < 6) {
+        errors.password = "Password must be at least 6 charachter"
+    } else {
+        delete errors.password
+    }
+
+    if(!data.confirmPassword) {
+        errors.confirmPassword = "Confirm the password"
+    } else if (data.confirmPassword !== password) {
+        errors.confirmPassword = "Password doesn't match"
+    } else {
+        delete errors.confirmPassword
+    }
+
+    if(data.isAccepted) {
+        delete errors.isAccepted
+    } else {
+        errors.isAccepted = "Accept our regulations"
+    }
+
+    return errors
+}
