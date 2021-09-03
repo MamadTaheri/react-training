191646c82f1603c9758d826badac0fa19d0e1439
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-04 00:10:14 +0430

    create login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
+import Login from './components/auth/login/Login'
 
-import SignUp from "./components/auth/signup/SignUp"
+// import SignUp from "./components/auth/signup/SignUp"
 
 
 const App = () => {
   return (
     <div className="App">
-      <SignUp />
+      {/* <SignUp /> */}
+      <Login />
     </div>
   );
 };
