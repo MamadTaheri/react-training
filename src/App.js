911e2dc163f6e193c785b62422f752ce71db3389
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-05 00:36:23 +0430

    Refresh project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import "./App.css";
-
-// Components
-import Landing from "./components/Landing";
-
 const App = () => {
   return ( 
      <div className="App">
-        <Landing />
+        App
      </div>
   );
 };
