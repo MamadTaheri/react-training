719d91e5a4bcf31f551ace04f24337eb4b8f3238
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-05 00:52:52 +0430

    add routing and Login component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
-import './index.css'
-
+import "./index.css";
 import App from "./App";
 
-ReactDom.render(<App />, document.getElementById("root"));
+import { BrowserRouter } from "react-router-dom";
+
+ReactDom.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
