315908ab5bd21d2e6c8a61d96b24a97676d56680
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-04 11:49:07 +0430

    add routing for SPA  + ProdeuctDetails component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css'
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 // Components
 import Store from './components/store/Store';
+import ProdeuctDetails from './components/productdetails/ProdeuctDetails';
+
 
 // Context
 import ProductContextProvider from './context/ProductContextProvider';
@@ -9,7 +12,12 @@ import ProductContextProvider from './context/ProductContextProvider';
 const App = () => {
   return (
     <ProductContextProvider>
-      <Store />
+      <Switch>
+        <Route path="/products/:id" component={ProdeuctDetails} />
+        <Route path="/products" component={Store} />
+        <Redirect to="/products" />
+      </Switch>
+
     </ProductContextProvider>
   );
 };
