8e9eb265aa6e0a8ed3d3e67e3601035f0c78749f
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-04 18:02:48 +0430

    Refresh Project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import "./App.css";
-import { Switch, Route, Redirect } from "react-router-dom";
-import Navbar from "./components/shared/Navbar";
-
-// Components
-import Store from "./components/store/Store";
-import ProdeuctDetails from "./components/productdetails/ProdeuctDetails";
-import ShopCart from "./components/shopcart/ShopCart";
-
-// Context
-import ProductContextProvider from "./context/ProductContextProvider";
-import CartContextProvider from "./context/CartContextProvider";
 
 const App = () => {
-  return (
-    <ProductContextProvider>
-      <CartContextProvider>
-        <Navbar />
-        <Switch>
-          <Route path="/products/:id" component={ProdeuctDetails} />
-          <Route path="/products" component={Store} />
-          <Route path="/cart" component={ShopCart} />
-          <Redirect to="/products" />
-        </Switch>
-      </CartContextProvider>
-    </ProductContextProvider>
+  return ( 
+     <div className="App">
+        App
+     </div>
   );
 };
 
