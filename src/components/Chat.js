8177c5ccbeefd3244ae01546c1ff472cc57c90aa
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-05 10:04:03 +0430

    initialising chat engine

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { auth } from "../services/firebase";
+import { useHistory } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
 
 // Components
 import Navbar from './Navbar';
@@ -7,9 +10,24 @@ import Navbar from './Navbar';
 import styles from './Chat.module.css'
 
 const Chat = () => {
+
+    const history = useHistory();
+
+    const logoutHandler = async () => {
+        await auth.signOut();
+        history.push("/")
+    }
+
     return (
         <div className={styles.container}>
-            <Navbar />
+            <Navbar logoutHandler={logoutHandler} />
+
+            <ChatEngine 
+                height="calc(100vh - 50px)"
+                projectID="dde4bb7a-34b3-46a1-937c-97aae97447cb"
+                userName="."
+                userSecret="."
+            />
         </div>
     );
 };
