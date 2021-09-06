d538a11120ae2ca9ae4b0eec8a28b4e21f6badc6
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-06 07:25:38 +0430

    add payload to reducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { increase, decrease } from '../redux/counter/counterAction';
 import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = (props) => {
 
+    const [value, setValue] = useState(1)
     const counter = useSelector(state => state.counterState.counter)
     const dispatch = useDispatch();
 
     return (
         <div>
             <h1>Counter - {counter} </h1>
-            <button onClick={() => dispatch(increase())} >Increase</button>
-            <button onClick={() => dispatch(decrease())} >Decrease</button>
+            <input type="text" value={value} onChange={(event) => setValue(event.target.value)} />
+            <button onClick={() => dispatch(increase(+value))} >Increase</button>
+            <button onClick={() => dispatch(decrease(+value))} >Decrease</button>
         </div>
     );
 };
