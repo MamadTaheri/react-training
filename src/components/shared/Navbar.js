18da071260e43f1dacd7519d9d960dd4e8a23dfe
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-06 14:06:37 +0430

    add redux to Product component

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,8 +1,6 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-
-// Context
-import { CartContext } from '../../context/CartContextProvider';
+import { useSelector } from 'react-redux';
 
 // Icons
 import shopIcon from "../../assets/icons/shop.svg";
@@ -12,7 +10,7 @@ import styles from "./Navbar.module.css";
 
 const Navbar = () => {
 
-    const {state} = useContext(CartContext);
+    const state = useSelector(state => state.cartState)
 
     return (
         <div className={styles.mainContainer}>
