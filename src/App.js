a4a426c6ca94cf7e69c85e965bc12de3d5c6f2b7
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-08 20:39:13 +0430

    using redux in component

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-       <Counter />
-    </div>
+        <Counter />
+      </div>
     </Provider>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { increase } from '../redux/counter/counterAction'; 
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
+
+    const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+
     return (
         <div>
-            <h1>Counter - </h1>
-            <button>Increase</button>
+            <h1>Counter - {counter}</h1>
+            <button onClick={() => dispatch(increase())}>Increase</button>
         </div>
     );
-};
+}; 
 
-export default Counter;
+export default Counter ;
