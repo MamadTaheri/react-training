15896bdc9e22003ec68b8c05e375c90c8b813675
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-08 19:48:40 +0430

    create store for Entire project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
+      <div className="App">
        <Counter />
     </div>
+    </Provider>
   );
 }
 
