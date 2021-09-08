816f3e5394618ea13fe0b48b8c12f7031bccf79d
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-08 16:55:43 +0430

    REfresh project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import "./App.css";
-import { Route, Switch, Redirect } from "react-router-dom";
-import { Provider } from "react-redux";
-
-// Components
-import Store from "./components/Store";
-import ProductDetails from "./components/ProductDetails";
-import Navbar from "./components/shared/Navbar";
-import ShopCart from "./components/ShopCart";
-
-// Redux
-import store from "./redux/store";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Navbar />
-      <Switch>
-        <Route path="/products/:id" component={ProductDetails} />
-        <Route path="/products" component={Store} />
-        <Route path="/cart" component={ShopCart} />
-        <Redirect to="/products" />
-      </Switch>
-    </Provider>
+    <div className="App">
+       Salam
+    </div>
   );
 }
 
