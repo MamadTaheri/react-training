7eb9cf8cf21c37c7a744d45a45211b0e612ddb6a
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-09 13:16:47 +0430

    Refresh project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import "./App.css";
-import Counter from "./components/Counter";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import Number from "./components/Number";
 
 function App() {
   return (
-    <Provider store={store}>
       <div className="App">
-        <Counter />
-        <Number />
+        App
       </div>
-    </Provider>
   );
 }
 
