9835b2f22acafb589ffd23f06f2cdfa9b80b1316
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-11 00:08:49 +0430

    using increase action in component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { increaseCounterAction } from '../redux/counter/counterAction';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Counter = () => {
+
+    const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+
     return (
         <div>
-            <h1>Counter ---= </h1>
-            <button>Increase</button>
+            <h1>Counter ---= {counter} </h1>
+            <button onClick={() => {dispatch(increaseCounterAction())}}>Increase</button>
         </div>
     );
 };
