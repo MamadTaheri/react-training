b8d74a9b66423252dee82c66490341254a933b85
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-11 11:47:20 +0430

    pass payload to redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { increaseCounterAction, decreaseCounterAction } from '../redux/counter/counterAction';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Counter = () => {
 
+    const [value, setValue] = useState(1)
     const counter = useSelector(state => state.counterState.counter);
     const dispatch = useDispatch();
 
     return (
         <div>
             <h1>Counter = {counter} </h1>
-            <button onClick={() => {dispatch(increaseCounterAction())}}>Increase</button>
-            <button onClick={() => {dispatch(decreaseCounterAction())}}>Decrease</button>
+            <input type="text" value={value} onChange={event => setValue(event.target.value) } />
+            <button onClick={() => {dispatch(increaseCounterAction(+value))}}>Increase</button>
+            <button onClick={() => {dispatch(decreaseCounterAction(+value))}}>Decrease</button>
         </div>
     );
 };
