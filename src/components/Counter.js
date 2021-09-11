fc477316f3b820d66fa36369966f8b00ffc2a97c
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-11 12:07:13 +0430

    add Ref to Counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import { increaseCounterAction, decreaseCounterAction } from '../redux/counter/counterAction';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Counter = () => {
 
+    const inputRef = useRef(null);
+
     const [value, setValue] = useState(1)
     const counter = useSelector(state => state.counterState.counter);
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        console.log(inputRef)
+        console.log(inputRef.current.type)
+        inputRef.current.focus()
+    }, [])
 
     return (
         <div>
@@ -14,6 +22,7 @@ const Counter = () => {
             <input type="text" value={value} onChange={event => setValue(event.target.value) } />
             <button onClick={() => {dispatch(increaseCounterAction(+value))}}>Increase</button>
             <button onClick={() => {dispatch(decreaseCounterAction(+value))}}>Decrease</button>
+            <input ref={inputRef} />
         </div>
     );
 };
