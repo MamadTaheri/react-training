2eed31d8c42497bcc9c7180885c3694e028960a4
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-11 11:23:27 +0430

    create numberAction

---
+++ src/redux/number/numberAction.js
@@ -0,0 +1,9 @@
+const increaseNumberAction = () => {
+    return {type:"INCREASE_NUMBER"}
+}
+
+const decreaseNumberAction = () => {
+    return {type:"DECREASE_NUMBER"}
+}
+
+export {increaseNumberAction, decreaseNumberAction};
