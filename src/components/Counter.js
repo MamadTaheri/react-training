b5db7c14de2b72ab0280fea2d7a75df575370a08
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-15 21:20:14 +0430

    implementing Delete logic

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   handleIncrement = () => {
@@ -12,7 +12,6 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        {this.props.children}
         <span className={this.getBadgeClassess()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
@@ -20,7 +19,10 @@ class Counter extends Component {
         >
           Increament
         </button>
-        <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
           Delete
         </button>
       </div>
