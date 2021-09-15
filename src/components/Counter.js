2f85640eea46ac8fc6670f05af3d8842c91a696b
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-15 20:14:30 +0430

    refactor Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,29 +3,22 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags:['tag1', 'tag2', 'tag3']
+    tags: ["tag1", "tag2", "tag3"],
   };
-  render() {
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
     return (
-        <>
-          <span className={this.getBadgeClassess()}>{this.formatCount()}</span>
-          <button className="btn btn-secondary btn-sm">Increament</button>
-          <ul>
-            {this.state.tags.map(tag => <li key={tag}>{tag }</li>)}
-          </ul>
-        </>
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  getBadgeClassess() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  render() {
+    return <>{this.renderTags()}</>;
   }
 }
 
