4ffe22163bba91d584f4ac5ef7e698d343c051b6
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-15 23:15:17 +0430

    convert Navbar to functional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
-
-class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-            </a>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
+const Navbar = (props) => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar{" "}
+            <span className="badge badge-pill badge-secondary">
+              {props.totalCounters}
+            </span>
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
