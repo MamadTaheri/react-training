fd1f1b66b183b1ad1c919fbc900edfce9b8b7e66
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-16 13:50:13 +0430

    create template structure

--- src/components/layout/Template.js
+++ src/components/layout/Template.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
 
-const Template = () => {
+const Template = (props) => {
     return (
         <div>
-            
+            <Header />
+            {props.children}
+            <Footer />
         </div>
     );
 };
