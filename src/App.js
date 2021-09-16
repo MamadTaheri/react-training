07cc3b8d1cad775c3897719122f5751efb8449f6
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-16 14:22:47 +0430

    create Main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Template from "./components/layout/Template";
+import Main from "./components/Main";
 
 const App = () => {
   return (
     <div>
       <Template>
-        <h1>Main Content</h1>
+        <Main />
       </Template>
     </div>
   );
