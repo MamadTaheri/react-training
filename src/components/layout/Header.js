91d8d42c0b9fb33469eb3b429081d2ab8a20d895
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-16 16:23:58 +0430

    update header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 const Header = () => {
     return (
-        <header className="bg-primary text-white p-3" style={{cursor:"pointer"}}>
-            Header
+        <header className="bg-primary p-3">
+            <Link className="text-white p-3" to="/">Home</Link>
+            <Link className="text-white p-3" to="/about">About</Link>
+            <Link className="text-white p-3" to="/posts">Posts</Link>
         </header>
     );
 };
