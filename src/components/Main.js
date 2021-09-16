bbee32d2fcc5199675b8a3a30cbf52d72820847f
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-16 14:36:34 +0430

    fetch data from api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Main = () => {
-    return (
-        <div>
-            <h1>Main Content</h1>
-        </div>
-    );
+  const [posts, setPosts] = useState([]);
+
+  const fetchDataFromServer = async () => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then(response => setPosts(response.data));
+    
+  };
+
+  useEffect(() => {
+    fetchDataFromServer();
+  }, []);
+
+  return (
+    <div>
+      {posts.length ? (
+        <ul className="d-flex flex-column text-center col-md-6">
+          {posts.map(post => <li className="bg-success text-white m-1" key={post.id}>{post.title}</li>)}
+        </ul>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
+  );
 };
 
 export default Main;
