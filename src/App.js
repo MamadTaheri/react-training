2ec25fcbcfb943327390d2f138a79c2b08838760
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-18 19:43:47 +0430

    refresh project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Header from "./components/layout/Header";
-import About from "./components/pages/About";
-import Home from "./components/pages/Home";
-import Posts from "./components/pages/Posts";
+import React, { Component } from 'react'
 
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
 
-
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route path="/about" component={About} />
-          <Route path="/posts" component={Posts} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
-};
-
-export default App;
