d41d0bbff554fdd2b167d1be992d98b17901732b
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-18 20:39:04 +0430

    add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react'
 
-import { Route, Switch, Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import AboutUs from './components/AboutUs'
 import Blogs from './components/Blogs'
 import HomePage from './components/HomePage'
+import Navbar from './components/Navbar'
 import Products from './components/Products'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <ul>
-          <li><Link to="/">Home page</Link></li>
-          <li><Link to="/blogs">Blogs</Link></li>
-          <li><Link to="/products">Products</Link></li>
-          <li><Link to="/aboutus">About Us</Link></li>
-        </ul>
+        <Navbar />
         <div>
           <Switch>
             <Route path="/blogs" component={Blogs} />
