06d312a2c7ab9836addf44439db0306c39ca8235
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-21 23:47:17 +0430

    refresh project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import React, { Component } from 'react'
-
-import { Route, Switch } from 'react-router-dom'
-import AboutUs from './components/AboutUs'
-import Blogs from './components/Blogs'
-import HomePage from './components/HomePage'
-import Navbar from './components/Navbar'
-import Products from './components/Products'
-import Product from './components/Product'
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-    return (
-      <div>
-        <Navbar />
-        <div>
-          <Switch>
-            <Route path="/products/:id" component={Product} />
-            <Route path="/blogs/:author?" render={(props) => <Blogs name="milad" {...props} /> } />
-            <Route path="/products" component={Products} />
-            <Route path="/aboutus" component={AboutUs} />
-            <Route path="/" component={HomePage} />
-          </Switch>
-        </div>
-      </div>
-    )
+    return <div></div>;
   }
 }
