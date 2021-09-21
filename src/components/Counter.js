8b1d9502a0edf2a55b59d486380bd1c2aa46f1fe
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-21 23:53:36 +0430

    change state with clickHandler

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,9 +4,14 @@ const Counter = () => {
 
     const [number, setNumber] = useState(0);
 
+    const clickHandler = () => {
+        setNumber(number + 1)
+    }
+
     return (
         <div>
             <h1>{number}</h1>
+            <button onClick={clickHandler}>Up one</button>
         </div>
     );
 };
