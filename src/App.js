8ddde1753524fe6fa1737efcedb4512fc1a001dc
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-30 11:20:14 +0330

    reset project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Counter from './components/Counter';
 
 const App = () => {
   return (
     <div>
-      <Counter />
+      App
     </div>
   );
 };
--- src/components/redux/counter/counterAction.js
+++
@@ -1,3 +0,0 @@
-export const increaseCounter = () => {
-    return {type: "INCREASECOUNTER"}
-}
