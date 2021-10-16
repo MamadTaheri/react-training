002f984a6b8af3143ca83677b76c371017a3054a
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-10-16 23:30:34 +0330

    get data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const App = () => {
-  return (
-    <div>
-    </div>
-  );
-};
+class App extends Component {
+
+  constructor(props){
+      super(props);
+      this.state = {
+         postData: []
+      }
+  } 
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(json => this.setState({
+      postData: json
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.getData}>get posts</button>
+        <h1>Posts : </h1>
+        {this.state.postData.map(post => <p key={post.id}>{post.id + " -> " + post.title}</p>)}
+      </div>
+    );
+  }
+}
 
 export default App;
