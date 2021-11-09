dab486a03f702693f8afa9c8df59f931421dd12c
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-11-09 23:09:40 +0330

    show data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import {carbrands} from './data/carbrands'
+
 const App = () => {
   return (
     <div>
-      
+      <ul>
+        {
+          carbrands.map((brand, index) => <li key={brand.id}>{`${index} --- ${brand.name} `}</li> )
+        }        
+      </ul>
     </div>
   );
 };
