5a2ee8303ca94b89d0fcfb5f2259d4b9ffd2d18c
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-18 13:41:02 +0330

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ProductList from "./components/product/ProductList";
 import CustomerList from "./components/customer/CustomerList";
 
 function App() {
+    const [showProductComponent, setShowProductComponent] = useState(true);
     return (
         <div>
             <h1>Hello World</h1>
             <hr />
-            <ProductList />
-            <hr />
-            <CustomerList />
+            <button className="btn btn-outline-primary mx-2" onClick={() => setShowProductComponent(true)}>Show Products</button>
+            <button className="btn btn-outline-primary mx-2" onClick={() => setShowProductComponent(false)}>Show Customers</button>
+            <br />
+            <br />
+            {showProductComponent ? <ProductList /> : <CustomerList />}
         </div>
     );
 }
