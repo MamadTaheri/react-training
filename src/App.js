040f7f7ecfc43f1e973b09a36d2d3d32f2798c68
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-20 21:46:41 +0330

    convert project links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ProductContainer from "./components/product/ProductContainer";
 import logo from '../src/assets/images/logo.JPG'
-import {BrowserRouter as Router, Link, Route, Routes, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, NavLink, Route, Routes, Switch} from 'react-router-dom'
 import {localRoutes} from "./globalData/localRoutes";
 import MainDashboard from "./components/dashboard/MainDashboard";
 
@@ -13,16 +13,14 @@ const App = () => {
                     <img src={logo} width="80" alt="logo" />
                 </a>
 
+
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-lg-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">داشبورد</a>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to={localRoutes.dashboard}>داشبورد</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">لیست محصولات</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">لیست مشتریان</a>
+                            <NavLink className="nav-link" to={localRoutes.products}>لیست محصولات</NavLink>
                         </li>
                     </ul>
                 </div>
