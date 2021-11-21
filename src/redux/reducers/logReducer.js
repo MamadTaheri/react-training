37dcb8f31da9559f74d3f90910d96b3884a7353c
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-21 23:14:56 +0330

    add reducers

---
+++ src/redux/reducers/logReducer.js
@@ -0,0 +1,11 @@
+const init = {
+    logs: []
+}
+
+const logReducer = (state = init, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+export default logReducer;
---
+++ src/redux/reducers/techReducer.js
@@ -0,0 +1,11 @@
+const init = {
+    techs: [],
+}
+
+const techReducer = (state = init, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+export default techReducer;
