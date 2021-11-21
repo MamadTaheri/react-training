cbdaa4ee3031fd0c6906483a2ea58ea6be764e1e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-21 23:15:14 +0330

    add store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ProductContainer from "./components/product/ProductContainer";
 import logo from '../src/assets/images/logo.JPG'
-import {BrowserRouter as Router, NavLink, Route, Routes, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, NavLink, Route, Routes} from 'react-router-dom'
 import {localRoutes} from "./globalData/localRoutes";
 import MainDashboard from "./components/dashboard/MainDashboard";
 import PageNotFound from "./components/page-not-found/PageNotFound";
+import {Provider} from "react-redux";
+import store from './redux/store'
 
 const App = () => {
     return (
         <Router>
             <nav className="navbar navbar-expand-xl navbar-light bg-light">
                 <a className="navbar-brand" href="#">
-                    <img src={logo} width="80" alt="logo" />
+                    <img src={logo} width="80" alt="logo"/>
                 </a>
 
 
@@ -27,11 +29,13 @@ const App = () => {
                 </div>
             </nav>
             <div className="container-fluid p-4">
-                <Routes>
-                    <Route path={localRoutes.dashboard} element={<MainDashboard /> } />
-                    <Route path={localRoutes.products} element={<ProductContainer /> } />
-                    <Route path="*" element={<PageNotFound />} />
-                </Routes>
+                <Provider store={store}>
+                    <Routes>
+                        <Route path={localRoutes.dashboard} element={<MainDashboard/>}/>
+                        <Route path={localRoutes.products} element={<ProductContainer/>}/>
+                        <Route path="*" element={<PageNotFound/>}/>
+                    </Routes>
+                </Provider>
             </div>
         </Router>
     );
