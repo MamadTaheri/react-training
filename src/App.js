fe68d5268d8483b2c40256b4a7cec1dc0138a7b4
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-25 19:15:10 +0330

    useLayoutEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import DeepDependenciy from "./components/DeepDependenciy";
+import React, {useEffect, useLayoutEffect} from 'react';
+// import DeepDependenciy from "./components/DeepDependenciy";
 // import Checkbox from "./components/Checkbox";
 // import DependencyComponent from "./components/DependencyComponent";
 
 const App = () => {
+
+    useEffect(() => console.log("useEffect"));
+    useLayoutEffect(() => console.log("useLayoutEffect"));
+
+
+
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-4 text-center">
+                    {console.log("render")}
                     <h3>App</h3>
                     <hr />
                     {/*<Checkbox />*/}
                     {/*<DependencyComponent />*/}
-                    <DeepDependenciy />
+                    {/*<DeepDependenciy />*/}
                 </div>
             </div>
         </div>
