4f03eabe040d260f8af137e30fd872c706e24a6a
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-26 23:30:49 +0330

    implement useFetch hook

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
-import React from 'react';
-import List from "./components/List";
-import faker from 'faker';
-import logo from '../src/assets/logo.png'
-import {FixedSizeList} from "react-window";
-
-const bigList = [...Array(5000)].map(() => ({
-    name: faker.name.findName(),
-    email: faker.internet.email(),
-    avatar: logo
-}));
+import React, {useState} from 'react';
+import GitHubUser from "./components/GitHubUser";
 
 const App = () => {
-    const renderRow = ({index, style}) => (
-        <div style={{...style, ...{display: "flex"} }}>
-            <img
-                src={bigList[index].avatar}
-                alt={bigList[index].name}
-                width={50}
-            />
-            <p>
-                {bigList[index].name} - {bigList[index].email}
-            </p>
-        </div>
-    );
-
-    // const renderItem = item => (
-    //     <div style={{ display: "flex"}}>
-    //         <img src={item.avatar} alt={item.name} width={50} />
-    //         <p>
-    //             {item.name} - {item.email}
-    //         </p>
-    //     </div>
-    // );
+    const [login, setLogin] = useState("moontahoe");
     return (
-        <FixedSizeList
-            height={window.innerHeight -20}
-            width={400 }
-            itemCount={bigList.length}
-            itemSize={50}
-        >
-            {renderRow}
-        </FixedSizeList>
+        <>
+            <GitHubUser login={login} />
+        </>
     );
 };
 
