ded5b03f0643d65fb29063ceae1973746e2c17d0
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-26 14:58:07 +0330

    work with api and localstorage

--- src/components/GitHubUser.jsx
+++ src/components/GitHubUser.jsx
@@ -1,10 +1,31 @@
 import React, {useEffect, useState} from 'react';
 
+const loadJson = key =>
+    key && JSON.parse(localStorage.getItem(key));
+
+
+const saveJson = (key, data) =>
+    localStorage.setItem(key, JSON.stringify(data));
+
+
 const GitHubUser = ({login}) => {
-    const [data, setData] = useState();
+    const [data, setData] = useState(loadJson(`user:${login}`));
+
+    useEffect(() => {
+        if(!data) return;
+        if (data.login === login) return;
+        const {name, avatar_url, location} = data;
+        saveJson(`user:${login}`, {
+            name,
+            login,
+            avatar_url,
+            location
+        });
+    }, [data]);
 
     useEffect(() => {
         if(!login) return;
+        if(data && data.login === login) return;
         fetch(`https://api.github.com/users/${login}`)
             .then(response => response.json())
             .then(setData)
@@ -12,7 +33,7 @@ const GitHubUser = ({login}) => {
     }, [login]);
 
     if(data)
-        return <pre>{JSON.stringify(data,null,2)}</pre>
+        return <pre>{JSON.stringify(data,null,6)}</pre>
 
     return null;
 };
