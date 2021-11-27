408e9b0427818fefc08dfe593c916a8d52da0eed
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-27 19:36:27 +0330

    more abstraction on custom hook

--- src/components/GitHubUser.jsx
+++ src/components/GitHubUser.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import {useFetch} from "../hooks/useFetch";
+import Fetch from "./Fetch";
 
-const GitHubUser = ( {login} ) => {
-    const {loading, data, error} = useFetch(`https://api.github.com/users/${login}`);
-    if(loading) return <p>Loading...</p>;
-    if(error)
-        return <pre>{JSON.stringify(error, null, 2)}</pre>;
+const UserDetails = ({ data }) => {
     return (
         <div>
-            <img
-                src={data.avatar_url}
-                alt={data.login}
-                style={{ width: 200}}
-            />
+            <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
             <div>
-                <h1>{data.login}</h1>
                 {data.name && <p>{data.name}</p>}
                 {data.location && <p>{data.location}</p>}
             </div>
         </div>
+    )
+}
+
+const GitHubUser = ( {login} ) => {
+    return (
+        <Fetch
+            uri={`https://api.github.com/users/${login}`}
+            renderSuccess={UserDetails}
+        />
     );
 };
 
