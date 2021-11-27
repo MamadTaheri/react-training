65767a8f1489339f81f5718aa94785b819e555f4
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-27 23:43:41 +0330

    canceling request using useMountedRef hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,17 +1,24 @@
 import {useState, useEffect} from "react";
+import {useMountedRef} from "./useMountedRef";
 
 export function useFetch(uri) {
     const [data, setData] = useState();
     const [error, setError] = useState();
     const [loading, setLoading] = useState(true);
 
+    const mounted = useMountedRef();
+
     useEffect(() => {
         if(!uri) return;
+        if (!mounted) return;
         fetch(uri)
             .then(date => date.json())
             .then(setData)
             .then(() => setLoading(false))
-            .catch(setError);
+            .catch(error => {
+                if (!mounted.current) return;
+                setError(error);
+            });
     },[uri]);
 
     return {
