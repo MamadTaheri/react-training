f453ff372d63594ab517ebce24c68f7956a4f9c2
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-28 23:38:17 +0330

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import SiteLayout from "./components/SiteLayout";
 
 const App = () => {
     return (
-        <div>
-            
-        </div>
+         <SiteLayout menu={<p>Menu</p>}>
+             <>
+                 <div className="callout">Callout</div>
+                 <h1>Contents</h1>
+                 <p>This is the main part of the example layout</p>
+             </>
+         </SiteLayout>
     );
 };
 
