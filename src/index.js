16c64b8bda67aa2db46eb93ad50022ce67ae6ec7
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-01 00:06:17 +0330

    refresh

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom";
 import  App  from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import Star from "./Star";
 
-ReactDOM.render(<Star /> , document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
