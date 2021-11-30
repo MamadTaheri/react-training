cd704142261e2cc06cf1c01e9933d2b8537e6ca5
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-30 23:28:10 +0330

    test Queries

--- src/Star.test.js
+++ src/Star.test.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import Star from "./Star";
-import {} from '@testing-library/jest-dom';
+import {render} from '@testing-library/react';
+import {} from "@testing-library/jest-dom"
 
 test("render first star", () => {
     const div = document.createElement("div");
@@ -12,4 +13,10 @@ test("render second star", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Star />, div);
     expect(div.querySelector("svg")).toHaveAttribute("id", "star");
+});
+
+test("renders an h1", () => {
+    const { getByText} = render(<Star />);
+    const h1 = getByText(/Great Star/);
+    expect(h1).toHaveTextContent("Great Star");
 });
