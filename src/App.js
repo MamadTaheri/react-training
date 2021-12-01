53e7e2cbac2aa7098dcf3d2bc853829826ae1b0f
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-01 22:58:15 +0330

    nested routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import {Link, Route, Routes} from "react-router-dom"
 import {About, Contact, Events, Home, Products} from "./pages";
 import Whoops404 from "./Whoops404";
+import Services from "./Services";
+import History from "./History";
 
 function App() {
     return (
          <div>
             <Routes>
                 <Route path="/" element={<Home />}/>
-                <Route path="/about" element={<About />}/>
+                <Route path="/about" element={<About />}>
+                    <Route path="services" element={<Services />}/>
+                    <Route path="history" element={<History />}/>
+                </Route>
                 <Route path="/events" element={<Events />}/>
                 <Route path="/products" element={<Products />}/>
                 <Route path="/contact" element={<Contact />}/>
--- src/pages.js
+++ src/pages.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 
 export function Home() {
     return (
@@ -14,6 +14,7 @@ export function About() {
     return (
         <div>
             <h1>[About]</h1>
+            <Outlet />
         </div>
     );
 }
