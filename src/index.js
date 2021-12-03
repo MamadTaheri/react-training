4d1968d50739b91f2275d9dca2a313390c9270da
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-03 23:39:03 +0330

    refresh

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./App";
+import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-// import {BrowserRouter} from "react-router-dom";
-import Menu from "./Menu";
-import {data} from "./data";
 
-ReactDOM.render(
-    <Menu title="Delicios Reciepe" recipes={data} />
-
-    , document.getElementById("root"));
+ReactDOM.render(<App/>  , document.getElementById("root"));
