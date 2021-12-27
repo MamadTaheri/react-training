709c0ec0549fcd88b5d45f2c01b01c17e0b45001
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-27 23:29:27 +0330

    refresh Entire project

--- src/ActionConstants.js
+++
@@ -1,5 +0,0 @@
-export const ActionConstants = {
-    INCREMENT: "INCREMENT",
-    DECREMENT: "DECREMENT",
-    RESET: "RESET",
-}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
-import Counter from "./components/Counter";
-import {ActionConstants} from "./ActionConstants";
+import App from "./App";
 
-import counter from "./reducres";
-
-const store = createStore(counter);
-const rootElement = document.getElementById("root");
-
-const render = () => ReactDOM.render(
-    <Counter
-        value = {store.getState()}
-        onIncrement = {() => store.dispatch({ type: ActionConstants.INCREMENT })}
-        onDecrement = {() => store.dispatch({ type: ActionConstants.DECREMENT })}
-    />, rootElement
-)
-
-render();
-
-store.subscribe(render)
-
+ReactDOM.render(<App />  , document.querySelector("#root") )
