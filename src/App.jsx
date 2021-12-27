47720d373436ea792e7af3b6f59af97a1661a729
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-28 00:01:31 +0330

    enhance SplitScreen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import SplitScreen from "./components/SplitScreen";
 
-const LeftHandComponent = () => {
-    return <h1 style={{backgroundColor: 'green'}}>Left!</h1>
+const LeftHandComponent = ({ name }) => {
+    return <h1 style={{backgroundColor: 'green'}}>{name}</h1>
 }
 
-const RighttHandComponent = () => {
-    return <h1 style={{backgroundColor: 'red'}}>Right!</h1>
+const RighttHandComponent = ({ message }) => {
+    return <h1 style={{backgroundColor: 'red'}}>{message}!</h1>
 }
 
 const App = () => {
     return (
-        <SplitScreen
-            left={LeftHandComponent}
-            right={RighttHandComponent}
-        />
+        <SplitScreen leftWeight={1} rightWeight={3}>
+            <LeftHandComponent name="Mamad" />
+            <RighttHandComponent message="Helo" />
+        </SplitScreen>
     );
 };
 
