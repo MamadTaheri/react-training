3e8e56ac145614b7c1013ae1c5a3b9dd4f17d7b9
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-28 13:13:36 +0330

    add data of people and products

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import SplitScreen from "./components/SplitScreen";
+import {people} from "./data/people";
+import {products} from "./data/products";
 
 const LeftHandComponent = ({ name }) => {
     return <h1 style={{backgroundColor: 'green'}}>{name}</h1>
@@ -7,11 +9,6 @@ const LeftHandComponent = ({ name }) => {
 const RighttHandComponent = ({ message }) => {
     return <h1 style={{backgroundColor: 'red'}}>{message}!</h1>
 }
-
-import {people} from "./data/people";
-import {products} from "./data/products";
-
-
 const App = () => {
     return (
         <SplitScreen leftWeight={1} rightWeight={3}>
