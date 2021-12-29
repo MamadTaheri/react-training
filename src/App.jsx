fbf4b19823b19b7bba1acf4e066e46220169f305
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-29 15:04:53 +0330

    use ResourceLoader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import CurrentUserLoader from "./components/CurrentUserLoader";
 import UserInfo from "./components/people/UserInfo";
 import {UserLoader} from "./components/UserLoader";
+import {ResourceLoader} from "./components/ResourceLoader";
+import ProductInfo from "./components/products/ProductInfo";
 
 const App = () => {
     return (
         <>
-            <UserLoader userId="123">
-                <UserInfo/>
-            </UserLoader>
-            <UserLoader userId="234">
-                <UserInfo/>
-            </UserLoader>
-            <UserLoader userId="345">
-                <UserInfo/>
-            </UserLoader>
+           {/*<ResourceLoader resourceUrl="http://localhost:3333/users/123" resourceName="user">*/}
+           {/*     <UserInfo />*/}
+           {/*</ResourceLoader>*/}
+            <ResourceLoader resourceUrl="http://localhost:3333/products/1" resourceName="product">
+                <ProductInfo />
+            </ResourceLoader>
         </>
     );
 };
