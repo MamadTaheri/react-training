aa6021b52f0269616fab31ea058f15d7d78eb2c2
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-29 15:16:37 +0330

    use DataSource component

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,32 @@ import UserInfo from "./components/people/UserInfo";
 import {UserLoader} from "./components/UserLoader";
 import {ResourceLoader} from "./components/ResourceLoader";
 import ProductInfo from "./components/products/ProductInfo";
+import {DataSource} from "./components/DataSource";
+import axios from "axios";
+
+const getSingleUserUrl = "http://localhost:3333/users/123";
+const getSingleProductUrl = "http://localhost:3333/products/1";
+
+const getServerData = async () => {
+    const response = await axios.get(getSingleUserUrl);
+    return response.data;
+}
 
 const App = () => {
     return (
         <>
-           {/*<ResourceLoader resourceUrl="http://localhost:3333/users/123" resourceName="user">*/}
+            <DataSource resourceName="user" getDataFunc={getServerData}
+            >
+                <UserInfo />
+            </DataSource>
+
+
+           {/*<ResourceLoader resourceUrl={getSingleUserUrl} resourceName="user">*/}
            {/*     <UserInfo />*/}
            {/*</ResourceLoader>*/}
-            <ResourceLoader resourceUrl="http://localhost:3333/products/1" resourceName="product">
-                <ProductInfo />
-            </ResourceLoader>
+           {/* <ResourceLoader resourceUrl={getSingleProductUrl} resourceName="product">*/}
+           {/*     <ProductInfo />*/}
+           {/* </ResourceLoader>*/}
         </>
     );
 };
