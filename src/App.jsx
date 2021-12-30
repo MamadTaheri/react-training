0c1a8c1ca679447eb1b2ef1e57185c06fb4dc09a
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-31 00:34:50 +0330

    collecting onboarding data

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,30 @@ import UncontrolledOnboardingFlow from "./components/ControlledVSUncontrolled/Un
 const StepOne = ({ goToNext }) => (
     <>
         <h1>Step 1</h1>
-        <button onClick={goToNext}>next</button>
+        <button onClick={() => goToNext({ name: 'Mamad Taheri' })}>next</button>
     </>
 );
 
 const StepTwo = ({ goToNext }) => (
     <>
         <h1>Step 2</h1>
-        <button onClick={goToNext}>next</button>
+        <button onClick={() => goToNext({ age: 100 })}>next</button>
     </>
 );
 const StepThree = ({ goToNext }) => (
     <>
         <h1>Step 3</h1>
-        <button onClick={goToNext}>next</button>
+        <button onClick={() => goToNext({ hairColor: 'black' })}>next</button>
     </>
 );
 
 const App = () => {
     return (
         <div>
-            <UncontrolledOnboardingFlow>
+            <UncontrolledOnboardingFlow onFinish={data => {
+                console.log(data);
+                alert('On Boarding data Completed successfully');
+            }}>
                 <StepOne />
                 <StepTwo />
                 <StepThree />
