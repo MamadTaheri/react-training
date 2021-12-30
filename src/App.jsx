442e68975f22557709889cb7822d211419382a5a
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-31 00:12:10 +0330

    ControlledModal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import UncontrolledModal from "./components/ControlledVSUncontrolled/UncontrolledModal";
+import React, {useState} from 'react';
+import ControlledModal from "./components/ControlledVSUncontrolled/ControlledModal";
 
 const App = () => {
+    const [shouldShowModal, setShouldShowModal] = useState(false);
+
     return (
         <>
-            <UncontrolledModal />
-            <button>Show Modal</button>
+            <ControlledModal
+                shouldShow={shouldShowModal}
+                onRequestClose={() => setShouldShowModal(false)} >
+                <h1>Salam be hame</h1>
+                <p>How Are you?</p>
+            </ControlledModal>
+            <button onClick={() => setShouldShowModal(!shouldShowModal)}>
+                {shouldShowModal ? 'Hide Modal' : 'Show modal'}
+            </button>
         </>
     );
 };
