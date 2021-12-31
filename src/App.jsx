bf7987595804c55110500ac24ae0bcd20753bb59
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-31 18:06:15 +0330

    partially Applied component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import RecursiveComponent from "./components/Functional Patterns/RecursiveComponent";
 import {nestedObject} from "./data/nestedObject";
 import {BigSuccessButton, DangerButton} from "./components/Functional Patterns/Composition";
+import {BigSuccessButton2, DangerButton2} from "./components/HOC/partiallyApply";
 
 const App = () => {
     return (
         <>
             <DangerButton text = "don't do it" />
             <BigSuccessButton text="Yes!!!!" />
+            <h1>--------------------------------</h1>
+            <DangerButton2 text = "Dont!" />
+            <BigSuccessButton2 text="Do!" />
         </>
     );
 };
