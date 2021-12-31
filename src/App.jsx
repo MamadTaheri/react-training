9f5b5490af4626c29f947a00a665a5c5a8fa8f8b
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-31 17:54:47 +0330

    component Composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import RecursiveComponent from "./components/Functional Patterns/RecursiveComponent";
 import {nestedObject} from "./data/nestedObject";
+import {BigSuccessButton, DangerButton} from "./components/Functional Patterns/Composition";
 
 const App = () => {
     return (
-         <RecursiveComponent data={nestedObject} />
+        <>
+            <DangerButton text = "don't do it" />
+            <BigSuccessButton text="Yes!!!!" />
+        </>
     );
 };
 
