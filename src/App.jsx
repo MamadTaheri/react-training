d47cdeb1907f8aef42fd85f810061ef11094eab4
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-31 17:41:56 +0330

    RecursiveComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import {UserInfoform} from "./components/HOC/UserInfoform";
-import UserInfo from "./components/people/UserInfo";
-import ProductInfo from "./components/products/ProductInfo";
-
+import RecursiveComponent from "./components/Functional Patterns/RecursiveComponent";
+import {nestedObject} from "./data/nestedObject";
 
 const App = () => {
     return (
-        <>
-            <UserInfo userId="123" />
-            <h1>-----------------------------------------------</h1>
-            <ProductInfo productId="0" />
-        </>
+         <RecursiveComponent data={nestedObject} />
     );
 };
 
