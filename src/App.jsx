f2d4b74efc786d573bac1108dcd341f8695058b5
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-01 23:29:30 +0330

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import RecursiveComponent from "./components/Functional Patterns/RecursiveComponent";
-import {nestedObject} from "./data/nestedObject";
-import {BigSuccessButton, DangerButton} from "./components/Functional Patterns/Composition";
-import {BigSuccessButton2, DangerButton2} from "./components/HOC/partiallyApply";
+import {people} from './data/people'
 
 const App = () => {
+    console.table(people)
     return (
-        <>
-            <DangerButton text = "don't do it" />
-            <BigSuccessButton text="Yes!!!!" />
-            <h1>--------------------------------</h1>
-            <DangerButton2 text = "Dont!" />
-            <BigSuccessButton2 text="Do!" />
-        </>
+        <div>
+            
+        </div>
     );
 };
 
--- src/components/people/SmallpersonListItem.jsx
+++
@@ -1,9 +0,0 @@
-const SmallpersonListItem = ({ person }) => {
-    const { name, age } = person;
-
-    return (
-        <p>Name: {name}, Age: {age} Years old</p>
-    );
-};
-
-export default SmallpersonListItem;
