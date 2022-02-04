11659caa6c68d7500c3ba3d16943c7e6dfc4ce80
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-04 20:55:54 +0330

    Product component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-// import React, { Component } from "react";
-
-// class component
-// class App extends Component {
-//     render() {
-//         return (
-//             <>
-//              <h1>Salam</h1>
-//              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae ea totam eveniet qui quasi ipsam ratione exercitationem ipsum eaque odit!</p>
-//             </>
-//         );
-//     }
-// }
-
-// functional component
-const App = () => {
-  return (
-    <>
-      <h1>Salam</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae ea
-        totam eveniet qui quasi ipsam ratione exercitationem ipsum eaque odit!
-      </p>
-    </>
-  );
-
-};
+import { Component } from "react";
+class App extends Component {
+    render() {
+        return (
+            <>
+             <h1>Salam</h1>
+             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae ea totam eveniet qui quasi ipsam ratione exercitationem ipsum eaque odit!</p>
+            </>
+        );
+    }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Product from "./components/Product";
 
-ReactDOM.render(<App />  , document.querySelector("#root") )
+ReactDOM.render(<Product />  , document.querySelector("#root") )
