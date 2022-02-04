b1ca77fafda6cc5e7e6d36ab984151c9131de883
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-04 20:04:38 +0330

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import {people} from './data/people'
+// import React, { Component } from "react";
 
+// class component
+// class App extends Component {
+//     render() {
+//         return (
+//             <>
+//              <h1>Salam</h1>
+//              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae ea totam eveniet qui quasi ipsam ratione exercitationem ipsum eaque odit!</p>
+//             </>
+//         );
+//     }
+// }
+
+// functional component
 const App = () => {
-    console.table(people)
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <h1>Salam</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae ea
+        totam eveniet qui quasi ipsam ratione exercitationem ipsum eaque odit!
+      </p>
+    </>
+  );
 };
 
 export default App;
