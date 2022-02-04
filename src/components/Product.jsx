30034731ab9054093a0a88f68d21487b45c941a9
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-04 23:00:32 +0330

    state

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,36 +1,29 @@
 import { Component } from "react";
 
 class Product extends Component {
-  count = 3;
-  list = ["item one", "item two", "item three"];
-  mappedList = this.list.map((item) => <li key={item}>{item}</li>);
+  state = {
+      count: 0
+  }
   render() {
     return (
       <>
-        {this.count ? (
-          <>
-            <span className="m-2 text-info">product name</span>
-            <span className="m-2 badge bg-primary">{this.format()}</span>
-            <span className="m-2 badge bg-secondary">
-              {this.count === 0 ? "zerooooo" : this.count}
-            </span>
-            <button onClick={this.handleIncrement.bind(this)} className="m-2 btn btn-sm btn-success">+</button>
-            <button onClick={this.handleDecrement} className="m-2 btn btn-sm btn-warning">-</button>
-            <button onClick={() => this.handleDelete(56)} className="m-2 btn btn-sm btn-danger">delete</button>
-            <ul>{this.mappedList}</ul>
-            <span style={{border:"2px solid blue", borderRadius:"50%", padding:"8px"}}>Salam</span>
-          </>
-        ) : <p>Zero found</p>}
+        <span className="m-2 text-info">laptop</span>
+        <span className="m-2 badge bg-primary">{this.format()}</span>
+        <button onClick={this.handleIncrement.bind(this)} className="m-2 btn btn-sm btn-success">+</button>
+        <button onClick={this.handleDecrement} className="m-2 btn btn-sm btn-warning">-</button>
+        <button onClick={() => this.handleDelete(56)} className="m-2 btn btn-sm btn-danger">delete</button>
       </>
     );
   }
 
   handleIncrement(){
-      console.log('increment', this)
+      const {count} = this.state;
+      this.setState({count: count + 1})
   }
   
   handleDecrement = () => {
-      console.log('decrement', this)
+    const {count} = this.state;
+    this.setState({count: count - 1})
   }
   
   handleDelete(itemnumber){
@@ -39,10 +32,10 @@ class Product extends Component {
   
 
   format() {
-    if (this.count == 0) {
+    if (this.state.count === 0) {
       return "Zero";
     } else {
-      return this.count;
+      return this.state.count;
     }
   }
 }
