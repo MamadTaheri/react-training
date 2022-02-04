595f22f7a21a31de319af03046e218bfde5d21a3
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-04 20:42:10 +0330

    clean-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 // import React, { Component } from "react";
-=======
-import React from 'react';
-import Counter from "./components/Counter";
-import { Provider } from "react-redux";
-import store from "./redux/store";
->>>>>>> 2f20597b5e852c11f3387fb546096d003a9c5e4e
 
 // class component
 // class App extends Component {
@@ -21,7 +14,6 @@ import store from "./redux/store";
 
 // functional component
 const App = () => {
-<<<<<<< HEAD
   return (
     <>
       <h1>Salam</h1>
@@ -31,16 +23,7 @@ const App = () => {
       </p>
     </>
   );
-=======
 
-    return (
-            <Provider store={store}>
-                <div style={{margin:"3rem"}}>
-                    <Counter />
-                </div>
-            </Provider>
-    );
->>>>>>> 2f20597b5e852c11f3387fb546096d003a9c5e4e
 };
 
 export default App;
