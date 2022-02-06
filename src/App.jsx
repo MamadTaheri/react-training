3b589fcc247aec234bd4c02a6551df78344d66e7
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-06 21:11:12 +0330

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,11 @@
-import { Component } from "react";
-import Navbar from "./components/class/Navbar";
-import Products from "./components/Products";
-import ProductContext from "./context/Products";
-class App extends Component {
-    constructor(props){
-        super(props);
-        console.log("App - constructor");
-    }
-    componentDidMount(){
-        console.log("App - componentDidMount");
-    }
-    componentDidUpdate(){
-        console.log("App - componentDidUpdate");
-    }
-    state = {
-        products:[
-            {id:1, count: 2, productName: 'laptop'},
-            {id:2, count: 1, productName: 'phone'},
-            {id:3, count: 4, productName: 'airpods'},
-        ]
-    }
+import React from 'react';
 
-    render() {
-        console.log("App - render");
-        return (
-            <>
-                <ProductContext.Provider 
-                    value={{
-                        products:this.state.products,
-                        onDelete :this.handleDelete,
-                        onIncrement :this.handleIncrement,
-                        onDecrement :this.handleDecrement,
-                        onReset :this.handleReset,
-                    }}>
-                <Navbar />
-                <Products  />
-                </ProductContext.Provider>
-            </>
-        );
-    }
-
-    handleDelete = (productId) => {
-        const filteredState = this.state.products.filter(q => q.id !== productId);
-        this.setState({products: filteredState})
-    }
-
-    handleIncrement = (productId) => {
-        const updatedState = [...this.state.products];
-        const index = updatedState.findIndex(q => q.id === productId);
-        updatedState[index].count +=1 ;
-        this.setState({products: updatedState});
-    }
-
-    handleDecrement = (productId) => {
-        const updatedState = [...this.state.products];
-        const index = updatedState.findIndex(q => q.id === productId);
-        updatedState[index].count -=1 ;
-        this.setState({products: updatedState});
-    }
-
-    handleReset = () => {
-        const updatedState = this.state.products.map(q => {
-            q.count = 0;
-            return q;
-        })
-        this.setState({products: updatedState});
-    }
-}
+const App = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
 
 export default App;
