07abe5257cd54251f501a947b22962ccf8b0e7ba
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-06 21:55:31 +0330

    Skeleton loading

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,33 +1,57 @@
+import axios from 'axios';
 import React, { Component } from 'react';
+import LoadingUsers from './loading/LoadingUsers';
 
 class Users extends Component {
+
     state = {
-        users: []
+        users: [],
+        isLoading: true
+    }
+
+    baseURL = "https://reqres.in/api/users/";
+
+    async componentDidMount() {
+        const response = await axios.get(this.baseURL);
+        setTimeout(() => {
+            this.setState({
+                users: response.data.data,
+                isLoading: false
+            });
+        }, 1200);
     }
 
     render() {
         return (
             <>
                <button onClick={this.handleCreate} className='btn btn-lg btn-primary'>create</button>
-               <div className="row">
+                <div className="container">
+                <div className="row">
                   {
-                      this.state.users.map(user => {
-                          return (
-                              <div className="col-4 text-center p-5">
-                                  <img src={user.avatar} style={{borderRadius: "50%", width: '100px'}} alt="" />
-                                  <h4>{user.first_name} {user.last_name}</h4>
-                                  <h5>{user.email}</h5>
-                                  <div className="row">
-                                      <div className="col-6">
-                                          <button onClick={this.handleUpdate} className="btn btn-info btn-sm">Update</button>
-                                          <button onClick={this.handleDelete} className="btn btn-danger btn-sm">Delete</button>
-                                      </div>
-                                  </div>
-                              </div>
-                          )
-                      })
+                    this.state.isLoading ? (
+                        <LoadingUsers />
+                    ) : (
+                        this.state.users.map(user => {
+                            return (
+                                <div className="col-4 text-center">
+                                    <img src={user.avatar} style={{borderRadius: "50%", width: '100px'}} alt="" />
+                                    <h4>{user.first_name} {user.last_name}</h4>
+                                    <h5>{user.email}</h5>
+                                    <div className="row">
+                                        <div className="col-6">
+                                            <button onClick={this.handleUpdate} className="btn btn-info btn-sm">Update</button>
+                                        </div>
+                                        <div className="col-6">
+                                            <button onClick={this.handleDelete} className="btn btn-danger btn-sm">Delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    )
                   } 
                </div> 
+                </div>
             </>
         );
     }
