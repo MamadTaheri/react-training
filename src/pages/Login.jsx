a24787fb1fbb63bafb1d0807e3643bab5977617f
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-07 23:30:50 +0330

    form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <h1>
-               Login Page 
-            </h1>
+            <form onSubmit={this.handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor='email'>Email:</label>
+                    <input id='email' className='form-control' type="text" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor='password'>Password:</label>
+                    <input id='password' className='form-control' type="text" />
+                </div>
+                <button className='btn btn-primary'>Login</button>
+            </form>
         );
     }
 }
