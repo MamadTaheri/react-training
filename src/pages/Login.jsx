76ede9ca9d3425fb37bdfd9c4c3fd08dc0d2b3c6
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-07 23:41:45 +0330

    using ref

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,24 @@
-import React, { Component } from 'react';
+import axios from 'axios';
+import React, { Component, createRef } from 'react';
 
 class Login extends Component {
+    email = createRef();
+    password = createRef();
+   
+    baseURL = "https://reqres.in/api/users";
 
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault();
+        console.log({email: this.email.current.value});
+        console.log({password: this.password.current.value});
+        const account = {
+            email: this.email.current.value,
+            password: this.password.current.value
+        }
+        if(account.email && account.password) {
+            const response = await axios.post(this.baseURL, account);
+            console.log(response);
+        }
     }
 
     render() {
@@ -11,11 +26,11 @@ class Login extends Component {
             <form onSubmit={this.handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor='email'>Email:</label>
-                    <input id='email' className='form-control' type="text" />
+                    <input ref={this.email} id='email' className='form-control' type="text" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor='password'>Password:</label>
-                    <input id='password' className='form-control' type="text" />
+                    <input ref={this.password} id='password' className='form-control' type="text" />
                 </div>
                 <button className='btn btn-primary'>Login</button>
             </form>
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,12 +1,11 @@
 import axios from "axios";
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React,{ useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import queryString from "query-string";
 
 const User = (props) => {
   const [user, setUser] = useState({});
+  const firstName = useRef(null);
   console.log(queryString.parse(props.location.search))
   const baseURL = "https://reqres.in/api/users";
 
@@ -14,6 +13,7 @@ const User = (props) => {
     (async () => {
       const response = await axios.get(`${baseURL}/${props.match.params.id}`);
       setUser(response.data.data);
+      console.log(firstName.current);
     })()
    }, [props.match.params.id]);
 
@@ -21,7 +21,7 @@ const User = (props) => {
     <>
       <img src={user.avatar} style={{ borderRadius: "50%", width: "100px" }} alt="" />
       <Link to={`/users/${user.id}`}>
-        <h4>  {user.first_name} {user.last_name} </h4>
+        <h4 ref={firstName}>{user.first_name} {user.last_name}</h4>
       </Link>
       <h5>{user.email}</h5>
       <button onClick={() => {props.history.push('/users')}} className="btn btn-info mt-3">Users</button>
