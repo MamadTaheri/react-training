eda3dc0d368c222aeedfa7deb1d88f302fd1e77b
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-12 00:25:15 +0330

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,10 @@
 import React from 'react';
-import Navbar from './UI/Navbar';
-import Users from './pages/Users';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import {Switch, Route, Redirect} from 'react-router-dom'
-import User from './pages/User';
-import NotFound from './pages/NotFound';
-import Dashboard from './pages/Dashboard';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import axios from 'axios';
-import Logout from './pages/Logout';
-import Protect from './components/Protect';
-
 
 const App = () => {
-    const [user, setUser] = useState(null);
-
-    const baseTokenURL = "https://reqres.in/api/userbytoken";
-
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if(!token){
-            setUser(null);
-            return;
-        }
-        // await axios.post(baseTokenURL, {token});
-        const response = {
-            data: {
-                user: {
-                    name: 'mamad',
-                    email: 'mamad@tmail.com'
-                }
-            }
-        }
-        if(!response.data.user){
-            setUser(null);
-            return;
-        }
-        setUser(response.data.user);
-    }, [])
     return (
-        <>
-            <Navbar user={user} />
-            <div className="container">
-                <Switch>
-                    <Route path='/users/:id' component={User} exact/>
-                    <Route path='/users' render={(props) => <Users {...props} />} exact/>
-                    <Route path='/login' component={Login} />
-                    <Route path='/logout' component={Logout} />
-                    <Route path='/register' component={Register} />
-                    <Protect path='/dashboard' component={Dashboard} />
-                    <Redirect from='/customers' to='/users' />
-                    <Route path='/not-found' component={NotFound}/>
-                    <Route path='/' component={Home} exact/>
-                    <Redirect to="/not-found" />
-                </Switch>
-            </div>
-        </>
+        <div>
+            
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>  
-    , document.querySelector("#root") )
+        <App />, document.querySelector("#root") )
