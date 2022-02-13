bfb0c666bdd82e7d82d156852e9d3aa78415743d
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-13 12:34:32 +0330

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { actionCreators } from './redux';
 
 const App = () => {
-
-    const account = useSelector((state) => state.account);
-    const dispatch = useDispatch();
-
-    const { depositMoney, withdrawMoney } = bindActionCreators(actionCreators, dispatch);
-
     return (
-        <div className='container border bordeer-primary'>
-            <h1>{account}</h1>
-            <button onClick={() => depositMoney(1000)}>Deposit</button>
-            <button onClick={() => withdrawMoney(500)}>Withdraw</button>
+        <div>
+
         </div>
     );
 };
