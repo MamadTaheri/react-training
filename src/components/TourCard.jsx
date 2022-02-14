2cf92730de40bd51f2a806fd6ea8faeff4db3e35
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-14 10:13:52 +0330

    update TourCard

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -2,7 +2,9 @@ import Paper from '@mui/material/Paper';
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import img1 from "../images/img1.jpg"
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import Rating from '@mui/material/Rating';
+import img1 from "../images/img1.jpg";
 
 const TourCard = (props) => {
     return (
@@ -13,7 +15,28 @@ const TourCard = (props) => {
                     <Typography variant="subtitle1" component="h2">
                         Immerse into the Falls
                     </Typography>
+                    <Box sx={{ display: "flex", alignItems: "center" }} >
+                        <AccessTimeIcon sx={{width: 12.5}} />
+                        <Typography variant="body2" component="p" marginLeft={0.5}>
+                            5 hours
+                        </Typography>
+                    </Box>
+                    <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+                        <Rating name="read-only" value={4.5} precision={0.25} size="small" readOnly />
+                        <Typography variant="body2" component="p" marginLeft={0.5}>
+                            4.5
+                        </Typography>
+                        <Typography variant="body3" component="p" marginLeft={1.5}>
+                            (655 reviews)
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant="h6" component="h3" marginTop={0}>
+                            From C $147
+                        </Typography> 
+                    </Box>
                 </Box>
+
             </Paper>
         </Grid>
     );
