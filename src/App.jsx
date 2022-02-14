b63a954b78f04ca7cd13d7940fc94cdb770c5b16
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-14 13:48:28 +0330

    add routing to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import React from "react";
-import TourCard from "./components/TourCard";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+import Home from "./pages/Home";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import MyAppBar from "./components/MyAppBar";
-import cities from "./data/data.json";
-import { Typography } from "@mui/material";
 
 const App = () => {
-  
-    console.log(cities);
 
   return (
-    <>
+    <BrowserRouter>
       <MyAppBar />
-      <Container sx={{ marginY: 5 }}>
-        {
-          cities.map(city => (
-            <>
-              <Typography variant="h4" component="h2" marginTop={5} marginBottom={3}>
-                  Top {city.name} Tours
-              </Typography>
-              <Grid container spacing={5}>
-                {
-                  city.tours.map((tour, index) => <TourCard key={index} tour={tour} />)
-                }
-              </Grid>
-            </>
-          ))
-        }
-      </Container>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
