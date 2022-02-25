52beb7ef85ac46cad8f5275fa9c0a8f7e8b03e95
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-26 00:02:57 +0330

    correct jsx syntax

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import "./index.css";
 import React from "react";
 
 export default function App() {
+  const buttonText = {id: 1, name: "mamad"};
+  
   return (
     <div>
-      <h1 className="title">Hi there!</h1>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      {/* <button style="background-color: blue; color: white">Submit</button> */}
+      <button style={{backgroundColor: "blue", color: "white", padding: "0 0.5rem"}}>
+        {/* {JSON.stringify(buttonText)} */}
+        {buttonText.name}
+      </button>
     </div>
   );
 }
