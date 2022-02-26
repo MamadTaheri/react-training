548434a18ba165bcf3b5504a3e691882c62aba6d
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-26 12:41:31 +0330

    create Approvalcard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Approvalcard from "./components/Approvalcard";
 import CommentDetail from "./components/CommentDetail";
 import java from "./images/java.png";
 import redux from "./images/redux.png";
@@ -6,10 +7,17 @@ import Typescript from "./images/Typescript.png";
 
 const App = () => {
   return (
-    <div className="ui container comments">
-      <CommentDetail author="mamad" avatar={java} timeAgo="1 Hours Ago" content="Nice blog post" />
-      <CommentDetail author="ali" avatar={redux} timeAgo="3 Hours Ago" content="I like violin" />
-      <CommentDetail author="zahra" avatar={Typescript} timeAgo="4 Hours Ago" content="I like driving" />
+    <div className="ui container comments" style={{marginTop: "1rem"}}>
+      <Approvalcard>
+        <CommentDetail author="mamad" avatar={java} timeAgo="1 Hours Ago" content="Nice blog post" />
+      </Approvalcard>
+      <Approvalcard>
+        <CommentDetail author="ali" avatar={redux} timeAgo="3 Hours Ago" content="I like violin" />
+      </Approvalcard>
+      <Approvalcard>
+        <CommentDetail author="zahra" avatar={Typescript} timeAgo="4 Hours Ago" content="I like driving" />
+      </Approvalcard>
+      <hr />
     </div>
   );
 };
