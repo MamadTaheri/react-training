7efb372ae4a08b3f4f9c5600c4ed5127244b9cfb
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-26 11:46:44 +0330

    add props to CommentDetail

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import React from 'react';
-import CommentDetail from './components/CommentDetail';
+import React from "react";
+import CommentDetail from "./components/CommentDetail";
 import java from "./images/java.png";
 import redux from "./images/redux.png";
 import Typescript from "./images/Typescript.png";
 
 const App = () => {
   return (
-    <div className='ui container comments'>
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+    <div className="ui container comments">
+      <CommentDetail author="mamad" avatar={java} timeAgo="1 Hours Ago" content="Nice blog post" />
+      <CommentDetail author="ali" avatar={redux} timeAgo="3 Hours Ago" content="I like violin" />
+      <CommentDetail author="zahra" avatar={Typescript} timeAgo="4 Hours Ago" content="I like driving" />
     </div>
   );
 };
--- src/components/CommentDetail.jsx
+++ src/components/CommentDetail.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import java from "../images/java.png";
 
-const CommentDetail = () => {
+const CommentDetail = ({ avatar, author, timeAgo, content }) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img src={java} alt="avatar" />
+        <img src={avatar} alt="avatar" />
       </a>
       <div className="content">
         <a href="/" className="author">
-          Mamad
+          {author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{timeAgo}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{content}</div>
       </div>
     </div>
   );
