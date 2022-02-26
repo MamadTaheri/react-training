b52ba0020651d1e20d8eacf9089bb603f3621083
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-26 12:46:25 +0330

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import React from "react";
-import Approvalcard from "./components/Approvalcard";
-import CommentDetail from "./components/CommentDetail";
-import java from "./images/java.png";
-import redux from "./images/redux.png";
-import Typescript from "./images/Typescript.png";
+import React from 'react';
 
 const App = () => {
   return (
-    <div className="ui container comments" style={{marginTop: "1rem"}}>
-      <Approvalcard>
-        <CommentDetail author="mamad" avatar={java} timeAgo="1 Hours Ago" content="Nice blog post" />
-      </Approvalcard>
-      <Approvalcard>
-        <CommentDetail author="ali" avatar={redux} timeAgo="3 Hours Ago" content="I like violin" />
-      </Approvalcard>
-      <Approvalcard>
-        <CommentDetail author="zahra" avatar={Typescript} timeAgo="4 Hours Ago" content="I like driving" />
-      </Approvalcard>
-      <hr />
+    <div>
+      
     </div>
   );
 };
