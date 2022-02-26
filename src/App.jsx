d3e9a9d4437f4664b59adc81f5a51fa032eb83cd
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-26 23:07:39 +0330

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import SeasonDisplay from './components/SeasonDisplay';
+import Spinner from './components/Spinner';
 
 class App extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state = { 
-      lat: null,
-      errorMessage: '' 
-    }
+  // constructor(props){
+  //   super(props);
+  //   this.state = { 
+  //     lat: null,
+  //     errorMessage: '' 
+  //   }
+  // }
+
+  state = {
+    lat: null,
+    errorMessage: ''
   }
  
   componentDidMount(){
@@ -17,17 +24,25 @@ class App extends React.Component {
     );
   }
 
-  render(){
-     
+  renderContent() {
+
     if(this.state.errorMessage && !this.state.lat){
-       return  <div>Error: {this.state.errorMessage}</div>
-     }
+      return  <div>Error: {this.state.errorMessage}</div>
+    }
 
-    if (!this.state.errorMessage && this.state.lat){
-       return <div>Latitude: {this.state.lat}</div>
-     }
+   if (!this.state.errorMessage && this.state.lat){
+      return <SeasonDisplay lat={this.state.lat} />
+    }
 
-     return <div>Loading!</div>
+    return <Spinner />
+  }
+ 
+  render(){
+      return (
+        <div className="border red">
+          {this.renderContent()}
+       </div>
+      )
   }
 };
 
