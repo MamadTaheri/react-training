9d9834f73d1e0ffe588813fec67d2c98dcc2bf73
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-01 18:16:31 +0330

    refresh project

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://api.unsplash.com",
-    headers: {
-        Authorization: 'Client-ID aMyK-ZH6NfTmy2CgDqx6UTYslgSS1577asIuQZr0vVs'
-    }
-})
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./App";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 ReactDOM.render(
-    <App/>, document.querySelector("#root")
+    <App />, document.querySelector("#root")
 );
