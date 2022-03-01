490b442fdb2bdd79be71485a45a48a503c988392
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-01 23:10:22 +0330

    refresh project

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,11 @@
-import React, { Component } from "react";
-import SearchBar from "./components/SearchBar";
-import youtube from "./apis/youtube";
-import VideoList from "./components/VideoList";
-import VideoDetail from "./components/VideoDetail";
+import React from 'react';
 
-class App extends Component {
-  state = { videos: [], selectedvideo: null };
-
-  componentDidMount() {
-    this.onTermSubmit('buildings');
-  }
-
-  onTermSubmit = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
-    this.setState({ 
-        videos: response.data.items, 
-        selectedvideo: response.data.items[0] ? response.data.items[0] : null
-    });
-  };
-
-  onVideoSelect = (video) => {
-    this.setState({ selectedvideo: video });
-  };
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar oFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedvideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
 
 export default App;
