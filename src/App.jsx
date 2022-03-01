1b4e83c51eb04e4200755e04d2e956cd079d4f5b
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-01 21:00:15 +0330

    add mode components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from "react";
+import SearchBar from "./components/SearchBar";
+import youtube from "./apis/youtube";
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
 
-const App = () => {
+class App extends Component {
+  state = { videos: [], selectedvideo: null };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedvideo: video })
+  };
+
+  render() {
     return (
-        <div>
-            
-        </div>
+      <div className="ui container">
+        <SearchBar oFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedvideo} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
+      </div>
     );
-};
+  }
+}
 
 export default App;
