9eb7f485e1826b818577a41840397144894b6b5e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-01 11:28:20 +0330

    add ImageCard component

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import ImageCard from './ImageCard';
+import './ImageList.css'
 
 const ImageList = (props) => {
     
     const images = props.images.map(image => {
-        return <img key={image.id} src={image.urls.full} alt={image.description} width={200} height={200} />
+        return <ImageCard key={image.id} image={image}  />
     })
 
     return (
-        <div style={{border: "1px solid red"}}>
+        <div className="image-list">
            {images}
         </div>
     );
