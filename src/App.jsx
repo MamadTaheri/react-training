84e3a1e6969316cf9812c03d60465d247fef61dd
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-05 18:50:10 +0330

    update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,62 @@
 import { useState } from "react";
-import Translate from "./components/Translate";
 import options from "./data/options";
+import items from "./data/items";
+import Translate from "./components/Translate";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Route from "./components/Route";
+
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+};
 
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
+      {/* {showAccordion()} */}
+      {/* {showList()} */}
+      {/* {showDropdown()} */}
+      {/* {showTranslate()}         */}
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChanged={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
         <Translate />
+      </Route>
     </div>
   );
 };
