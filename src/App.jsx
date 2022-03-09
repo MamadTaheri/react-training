0f3c1457fd3a3dcbdcdcf4b9cd01c62bb2c4f958
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-10 00:19:42 +0330

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
-import { useState } from "react";
-import options from "./data/options";
-import items from "./data/items";
-import Translate from "./components/Translate";
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
-import Dropdown from "./components/Dropdown";
-import Route from "./components/Route";
-import Header from "./components/Header";
+import React from 'react';
 
 const App = () => {
-  const [selected, setSelected] = useState(options[0]);
-
   return (
     <div>
-      <Header />
-      <Route path="/">
-        <Accordion items={items} />
-      </Route>
-      <Route path="/list">
-        <Search />
-      </Route>
-      <Route path="/dropdown">
-        <Dropdown
-          label="Select a color"
-          options={options}
-          selected={selected}
-          onSelectedChanged={setSelected}
-        />
-      </Route>
-      <Route path="/translate">
-        <Translate />
-      </Route>
+      
     </div>
   );
 };
