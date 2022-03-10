d4ad545fed2be56e325975fde45e98cf3752c250
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-10 22:30:26 +0330

    how to cache the results using useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import Counter from './components/Counter';
-import Counter2 from './components/Counter2';
-import GetData from './components/GetData';
+import { useMemo, useState } from "react";
 
 const App = () => {
+  const [isShown, setIsShown] = useState(true);
+  const [number , setNumber] = useState(0);
 
+  const slow = useMemo(() => {
+    for (let i = 0; i <1000000000; i++) {}
+    return isShown;
+  }, [isShown])
+
+  const showHandler = () => {
+    setIsShown(!isShown);
+  }
+
+  const numberHandler = () => {
+    setNumber(prevNumber => prevNumber + 1);
+  }
+
+  console.log(number)
 
   return (
-    <div>
-      {/* <Counter /> */}
-      {/* <hr /> */}
-      {/* <Counter2 /> */}
-      <GetData />
+    <div className="container text-center">
+      <button className="btn btn-primary" onClick={showHandler}>show-hide</button>
+      <span>{slow ? "Show" : "Hide"}</span>
+      <br />
+      <br />
+      <button className="btn btn-info" onClick={numberHandler}>+</button>
+      <span>{number}</span>
     </div>
   );
 };
