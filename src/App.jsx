c181053ed3cddcf1b831c2310a63f7f2ef977cc4
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-11 00:35:22 +0330

    using redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import Counter from './components/Counter';
+import React from "react";
+import Counter from "./components/Counter";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const App = () => {
   return (
-    <div>
-      <Counter />
-    </div>
+    <Provider store={store}>
+      <div>
+        <Counter />
+      </div>
+    </Provider>
   );
 };
 
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { increaseCounter } from "../redux/counter/counterAction";
+import {connect } from "react-redux";
 
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div>
-            <h1>Counter - </h1>
-            <button>Increase</button>
+            <h1>Counter - {props.counterState} </h1>
+            <button onClick={props.increaseCounter}>Increase</button>
         </div>
     );
 };
 
-export default Counter;
+const mapStateToProps = globalState => {
+    return {
+        counterState: globalState.counter
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        increaseCounter: () => dispatch(increaseCounter())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
