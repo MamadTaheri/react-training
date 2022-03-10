dc6c6c394426f2476f3af7bf5a7879377011bdc1
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-10 23:37:00 +0330

    useLocalStorage custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import React, {   useState } from "react";
-import Login from "./components/Login";
-import Landing from "./components/Landing";
+import React from 'react';
+import useLocalStorage from './hooks/useLocalStorage';
 
 const App = () => {
-  // const inputRef = useRef(null);
-  const [toggle, setToggle] = useState(true);
 
-  console.log("*******App Render**********");
-  // console.log(inputRef);
-
-  // useLayoutEffect(() => {
-  //   console.log("useLayoutEffect");
-  //   console.log(inputRef);
-
-  //   return () => {
-  //     console.log("useLayoutEffect cleanup");
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log("useEffect");
-  //   console.log(inputRef);
-
-  //   return () => {
-  //     console.log("useEffect cleanup");
-  //   }
-  // }, []);
+  const [name, setName] = useLocalStorage("name", "");
 
   return (
     <div>
-      {/* <input type="text" ref={inputRef} /> */}
-      <button onClick={() => setToggle(!toggle)}>Toggle</button>
-       {toggle ? <Landing /> : <Login />}
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
     </div>
   );
 };
