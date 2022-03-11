5beba6d33d204846d63c7c0f5b296fe0931fbcd8
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-11 22:02:27 +0330

    working with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import UseEffectComponent from "./components/02-useEffect/UseEffectComponent";
 // import UseStateComponent from './components/01-useState/UseStateComponent';
-import UseReducerComponent01 from "./components/04-useReducer/UseReducerComponent01";
-import UseReducerComponent02 from "./components/04-useReducer/UseReducerComponent02";
+// import UseReducerComponent01 from "./components/04-useReducer/UseReducerComponent01";
+// import UseReducerComponent02 from "./components/04-useReducer/UseReducerComponent02";
 
 const App = () => {
   return (
     <div className="container text-center border border-primary rounded p-2 ">
       <div className="row">
         {/* <UseStateComponent /> */}
-        <UseReducerComponent01 />
-        <UseReducerComponent02 />
+        {/* <UseReducerComponent01 /> */}
+        {/* <UseReducerComponent02 /> */}
+        <UseEffectComponent />
       </div>
     </div>
   );
