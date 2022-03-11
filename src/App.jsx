e03a1e0f6f4894152ee37898b80720c4d91610ed
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-11 21:34:03 +0330

    working with useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import UseStateComponent from './components/01-useState/UseStateComponent';
+import React from "react";
+// import UseStateComponent from './components/01-useState/UseStateComponent';
+import UseReducerComponent01 from "./components/04-useReducer/UseReducerComponent01";
+import UseReducerComponent02 from "./components/04-useReducer/UseReducerComponent02";
 
 const App = () => {
   return (
-    <div className="container text-center border border-primary rounded p-5 ">
-       <UseStateComponent />
+    <div className="container text-center border border-primary rounded p-2 ">
+      <div className="row">
+        {/* <UseStateComponent /> */}
+        <UseReducerComponent01 />
+        <UseReducerComponent02 />
+      </div>
     </div>
   );
 };
