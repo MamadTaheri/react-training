4b0d571b9798ef3d34df37f9391dbd86d2c4385e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-12 23:28:28 +0330

    working with themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Button, makeStyles } from "@material-ui/core";
+import { Person, TramSharp } from "@material-ui/icons";
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    color: "yellow",
+    backgroundColor: theme.palette.success.dark
+  }
+}));
+
 const App = () => {
+  const classes = useStyles();
   return (
     <div>
-      App
+      <Button variant="contained" color="primary" startIcon={<Person />}>سلام</Button>
+      <Button variant="contained" color="secondary" startIcon={<TramSharp />}>علیک</Button>
+      <Button variant="outlined" color="primary" style={{color: 'black'}}>محمد</Button>
+      <Button variant="outlined" color="secondary" className={classes.button}>طاهری</Button>
     </div>
   );
 };
