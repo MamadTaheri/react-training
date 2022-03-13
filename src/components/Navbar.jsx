8c1dff3adb0a6f3d1fe22489c2a1496235b223a4
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-13 21:47:17 +0330

    extend Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import {
   alpha,
   AppBar,
+  Avatar,
+  Badge,
   InputBase,
   makeStyles,
   Toolbar,
   Typography,
 } from "@material-ui/core";
-import { Search } from "@material-ui/icons";
+import { Cancel, Mail, Notifications, Search } from "@material-ui/icons";
+import { useState } from "react";
+import a from "../assets/a.jpg"
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -34,15 +38,38 @@ const useStyles = makeStyles((theme) => ({
     },
     borderRadius: theme.shape.borderRadius,
     width: "50%",
+    [theme.breakpoints.down("sm")]: {
+      display: (props) => props.open ? "flex" : "none",
+      width: "70%",
+    },
   },
   input: {
       color: "white",
       marginLeft: theme.spacing(1),
+  },
+  cancel: {
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    }
+  },
+  searchButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display:  "none",
+    },
+  },
+  icons: {
+    alignItems: "center",
+    display: (props) => props.open ? "none" : "flex",
+  },
+  badge: {
+    marginRight: theme.spacing(2),
   }
 }));
 
 const Navbar = () => {
-  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const classes = useStyles({ open });
 
   return (
     <AppBar>
@@ -56,8 +83,18 @@ const Navbar = () => {
         <div className={classes.searchBox}>
           <Search />
           <InputBase placeholder="Search..." className={classes.input} />
+          <Cancel className={classes.cancel} onClick={() => setOpen(false)} />
         </div>
-        
+        <div className={classes.icons}>
+          <Search className={classes.searchButton} onClick={() => setOpen(true)} />
+          <Badge badgeContent={4} color="secondary" className={classes.badge}>
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="secondary" className={classes.badge}>
+            <Notifications />
+          </Badge>
+          <Avatar alt="mamad Taheri" src={a} />
+        </div>
       </Toolbar>
     </AppBar>
   );
