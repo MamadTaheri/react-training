b557f4fc893160699f1f0de8c7b9c3547bc91045
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-15 00:08:57 +0330

    init new project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import MainPage from './components/MainPage';
-import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <MainPage />
+      App
     </div>
   );
 };
