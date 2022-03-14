0059b32fb06f1741fd1b8661031fb1b77815176f
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-14 20:59:18 +0330

    update

---
+++ src/components/Add.jsx
@@ -0,0 +1,144 @@
+import {
+  Button,
+  Container,
+  Fab,
+  FormControlLabel,
+  FormLabel,
+  makeStyles,
+  MenuItem,
+  Modal,
+  Radio,
+  RadioGroup,
+  Snackbar,
+  TextField,
+  Tooltip,
+} from "@material-ui/core";
+import { Add as AddIcon } from "@material-ui/icons";
+import { useState } from "react";
+
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: "fixed",
+    bottom: 10,
+    right: 10,
+  },
+  container: {
+    width: 500,
+    height: 550,
+    backgroundColor: "white",
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    margin: "auto",
+    [theme.breakpoints.down("sm")]: {
+      width: "100vw",
+      height: "100vh",
+    },
+  },
+  form: {
+    padding: theme.spacing(2),
+  },
+  item: {
+    marginBottom: theme.spacing(3),
+  },
+}));
+
+const Add = () => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  // const handleClose = (event, reason) => 
+  return (
+    <>
+      <Tooltip title="افزودن" aria-label="افزودن" onClick={() => setOpen(true)}>
+        <Fab color="primary" className={classes.fab}>
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+      <Modal open={open}>
+        <Container className={classes.container}>
+          <form className={classes.form} autoComplete="off">
+            <div className={classes.item}>
+              <TextField
+                id="standard1"
+                label="Title"
+                size="small"
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className={classes.item}>
+              <TextField
+                id="outlined1"
+                multiline
+                rows={4}
+                defaultValue="Tell your story..."
+                variant="outlined"
+                label="Description"
+                size="small"
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className={classes.item}>
+              <TextField select label="Visibility" value="Public">
+                <MenuItem value="Public">Public</MenuItem>
+                <MenuItem value="Private">Private</MenuItem>
+                <MenuItem value="Unlisted">Unlisted</MenuItem>
+              </TextField>
+            </div>
+            <div className={classes.item}>
+              <FormLabel component="legend">Who can comment ?</FormLabel>
+              <RadioGroup>
+                <FormControlLabel
+                  value="Everybody"
+                  control={<Radio size="small" />}
+                  label="Everybody"
+                />
+                <FormControlLabel
+                  value="My Friends"
+                  control={<Radio size="small" />}
+                  label="My Friends"
+                />
+                <FormControlLabel
+                  value="Custom"
+                  control={<Radio size="small" />}
+                  label="Custom (Premium)"
+                />
+                <FormControlLabel
+                  value="disabled"
+                  disabled
+                  control={<Radio />}
+                  label="(Disabled option)"
+                />
+              </RadioGroup>
+            </div>
+            <div className={classes.item}>
+              <Button
+                variant="outlined"
+                color="primary"
+                style={{ marginRight: 20 }}
+              >
+                Create
+              </Button>
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={() => setOpen(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </Container>
+      </Modal>
+      {/* <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}> */}
+        {/* <Alert onClose={handleClose} severity="success"> */}
+          This is a success message!
+        {/* </Alert> */}
+      {/* </Snackbar> */}
+    </>
+  );
+};
+
+export default Add;
