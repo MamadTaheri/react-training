b161dd337e1c64ea3a66850ad1232b36c5b0def5
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-04-22 10:02:28 +0430

    add bootstrap library

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Provider } from "react-redux";
 import store from "./stateManagement/store";
